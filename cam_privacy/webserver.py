8aa91c9b23e923c38e9290d843b236eabee1af5e
Jesse Schoepfer <jelloeater@gmail.com>
2023-05-21 00:47:12 -0400

    Simplifed return

--- cam_privacy/webserver.py
+++ cam_privacy/webserver.py
@@ -25,17 +25,11 @@ class web_app:
 
         @self.app.get("/{hostname}/on")
         async def privacy_on(hostname: str):
-            s = cam_privacy.main.privacy_toggle_single(hostname, True)
-            r = fastapi.Response()
-            r.body(s)
-            return r
+            return cam_privacy.main.privacy_toggle_single(hostname, True)
 
         @self.app.get("/{hostname}/off")
         async def privacy_off(hostname: str):
-            s = cam_privacy.main.privacy_toggle_single(hostname, False)
-            r = fastapi.Response()
-            r.body(s)
-            return r
+            return cam_privacy.main.privacy_toggle_single(hostname, False)
 
 
 class Server:
